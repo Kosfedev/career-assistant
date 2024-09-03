0f25d06f4a5432e5387a60dfad8eaeacc163149e
Fedor Kostrov <kostrov.f.dev@gmail.com>
2024-09-03 20:05:44 +0300

    port fix

--- src/shared/config/backend.ts
+++ src/shared/config/backend.ts
@@ -1,2 +1,2 @@
 export const HH_END_POINT = 'https://api.hh.ru';
-export const BACKEND_END_POINT = 'https://nasty-sole-career-assistant-61040774.koyeb.app:7000';
+export const BACKEND_END_POINT = 'https://nasty-sole-career-assistant-61040774.koyeb.app';
